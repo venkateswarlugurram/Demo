c545eaa65cf336ba9f20f71df2d1d315eafc00fa
venkateswarlu <gurramvenkateswarlu@yahoo.com>
2018-04-16 14:16:59 +0530

    transferpage added

---
+++ src/main/java/Homepage/TranaferPage.java
@@ -0,0 +1,9 @@
+package Homepage;
+
+public class TranaferPage {
+
+	public static void main(String[] args) {
+System.out.println("Trasfer page test");
+	}
+
+}
---
+++ src/test/java/HomepageTest/TransferPageTest.java
@@ -0,0 +1,10 @@
+package HomepageTest;
+
+public class TransferPageTest {
+
+	public static void main(String[] args) {
+
+		System.out.println("tranferpage test successful.... ");
+	}
+
+}
